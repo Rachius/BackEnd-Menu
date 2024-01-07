0a3c838a4d03733aa1ebf5286537611a228f344c
Juan Pablo Moya <juanpablo785@hotmail.com>
2024-01-07 00:57:10 -0300

    Crecion del token + encriptacion de clave

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,23 +1,38 @@
 import Usuario from "../models/user.model.js"
-
+import bcrypt from 'bcryptjs'
+import {createAccessToken} from '../libs/jwt.js'
 
 export const register = async (req,res) => {
         const {username,email,password} = req.body
 
         try {
+
+                const passwordHash = await bcrypt.hash(password,10)
+
                 const newUser = new Usuario({
                         username,
                         email,
-                        password
+                        password: passwordHash
                 })
+                
+                
         
         
         
-        
-                const userSaver = await newUser.save()
-                res.json(userSaver)
+                const userSaved = await newUser.save()
+                const token = await createAccessToken({id: userSaved._id})
+                res.cookie("token",token)
+                res.json({
+                        id: userSaved._id,
+                        username : userSaved.username,
+                        email : userSaved.email,
+
+               })
+
+
+
         } catch (error) {
-                console.log(error)
+                res.status(500).json({message: error.message})
         }
 
        
